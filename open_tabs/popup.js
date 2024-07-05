00591cc9813610d9eae9afdfa8d8cf868936ce1a
TUC117 <22b0913@iitb.ac.in>
2024-07-05 17:11:25 +0530

    Open tabs v1.1

--- open_tabs/popup.js
+++ open_tabs/popup.js
@@ -1,11 +1,29 @@
-document.getElementById('openlinks').addEventListener('click', () => {
-    const links = [
-        "https://campus.placements.iitb.ac.in/",
-        'https://webmail.iitb.ac.in/'
-    ];
+const links = [
+    "https://campus.placements.iitb.ac.in/",
+    'https://webmail.iitb.ac.in/'
+];
 
+document.getElementById('openlinks').addEventListener('click', () => {
     links.forEach(link => {
         chrome.tabs.create({url:link});
     });
 });
 
+document.getElementById('addlinks').addEventListener('click', () => {
+    const newlink = prompt("enter url:");
+    if(isvaild(newlink)){
+        links.push(newlink);
+        alert(`link added: ${newlink}`);
+    }else{
+        alert('not valid');
+    }
+});
+
+function isvaild(string){
+    try{
+        new URL(string);
+        return true;
+    } catch (_){
+        return false;
+    }
+}
